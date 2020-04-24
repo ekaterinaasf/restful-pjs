8fe677a4224d1756ba9d23dfdea709ad0e419d70
Ekaterina <nataveres@gmail.com>
2020-04-24 23:55:39 +0200

    Try1

--- public/script.js
+++ public/script.js
@@ -4,12 +4,25 @@ class fetcher {
     const start = Date.now();
     const timedMsg = (msg) => `${Date.now() - start} ms.: ${msg}`;
   }
-
   async fetching0() {
     const res = await fetch("http://127.0.0.1:3000/todos");
     const data = await res.json();
     console.log("Step " + i);
     //rez1();
+    this.posting(data);
+  }
+  posting(data) {
+    let output = "<h2>ToDos</h2>";
+    data.forEach(function (el) {
+      output += `
+            <ul>
+              <li>id: ${el.id}</li>
+              <li>todoText: ${el.todoText}</li>
+              <li>completed: ${el.completed}</li>
+            </ul>
+          `;
+    });
+    document.getElementById("output").innerHTML = output;
   }
 }
 
@@ -24,5 +37,5 @@ function rez1() {
 let name = "ui.fetching" + i;
 butEl.addEventListener("click", function (event) {
   event.preventDefault();
-  ui.name();
+  ui.fetching0();
 });
